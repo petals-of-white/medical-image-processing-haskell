dcedf034bb66f267c2ec982c9321d05587176863
Maksym Syvash <maxim.sivash@gmail.com>
2025-01-23 18:13:12 +0200

    added some basic files

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,57 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
-import Lib
+module Main where
+
+import           Control.Lens
+import           Monomer
+import           TextShow
+
+newtype AppModel = AppModel {
+  _clickCount :: Int
+} deriving (Eq, Show)
+
+data AppEvent
+  = AppInit
+  | AppIncrease
+  deriving (Eq, Show)
+
+makeLenses 'AppModel
+
+buildUI
+  :: WidgetEnv AppModel AppEvent
+  -> AppModel
+  -> WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree where
+  widgetTree = vstack [
+      label "Hello world",
+      spacer,
+      hstack [
+        label $ "Click count: " <> showt (model ^. clickCount),
+        spacer,
+        button "Increase count" AppIncrease
+      ]
+    ] `styleBasic` [padding 10]
+
+handleEvent
+  :: WidgetEnv AppModel AppEvent
+  -> WidgetNode AppModel AppEvent
+  -> AppModel
+  -> AppEvent
+  -> [AppEventResponse AppModel AppEvent]
+handleEvent wenv node model evt = case evt of
+  AppInit     -> []
+  AppIncrease -> [Model (model & clickCount +~ 1)]
 
 main :: IO ()
-main = someFunc
+main = do
+  startApp model handleEvent buildUI config
+  where
+    config = [
+      appWindowTitle "Tutorial 01 - Basics",
+      appWindowIcon "./assets/images/icon.png",
+      appTheme darkTheme,
+      appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
+      appInitEvent AppInit
+      ]
+    model = AppModel 0
