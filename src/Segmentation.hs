97a6ddcfbf7710ee1cc25d37c0871c1ea4725f4b
Maksym Syvash <maxim.sivash@gmail.com>
2025-02-20 19:08:16 +0200

    working on segmentation

--- src/Segmentation.hs
+++ src/Segmentation.hs
@@ -1,31 +1,85 @@
 module Segmentation where
 
 import Graphics.Gloss.Data.QuadTree
-import Vision.Image
+import Vision.Image as I
 import Vision.Primitive
+import qualified Data.Vector.Storable as VS
 
+import qualified Control.Foldl as Foldl
+import Graphics.Gloss.Data.Extent 
+import Graphics.Gloss.Data.Quad
+import Data.Maybe
+import Vision.Image.JuicyPixels
+
+import Codec.Picture.Bitmap (writeBitmap)
+
+instance Functor (QuadTree) where
+    fmap f (TLeaf a) = TLeaf (f a)
+    fmap f (TNode nw ne sw se) = TNode (fmap f nw) (fmap f ne) (fmap f sw) (fmap f se)
 
 isHomogeneous :: Manifest Int -> Bool
-isHomogeneous = undefined
+isHomogeneous img = Foldl.fold Foldl.std (Prelude.map fromIntegral $ VS.toList $ manifestVector img) <= 7
+
+areHomogeneous :: Manifest Int -> Manifest Int -> Bool
+areHomogeneous = undefined
+
+treeToImage :: QuadTree Int -> Size -> Manifest Int
+treeToImage quadTree size@(Z :. h :. w) = fromFunction size $ fromMaybe 0 . getPixel quadTree (makeExtent h 0 w 0) . swapY
+    where
+        getPixel (TLeaf n) extent coord = if coordInExtent extent coord then Just n else Nothing
+        getPixel (TNode nw ne sw se) extent coord =
+            case quadOfCoord extent coord of
+                Just pos -> 
+                    case pos of
+                        NW -> getPixel nw (cutQuadOfExtent pos extent) coord
+                        NE -> getPixel ne (cutQuadOfExtent pos extent) coord
+                        SE -> getPixel se (cutQuadOfExtent pos extent) coord
+                        SW -> getPixel sw (cutQuadOfExtent pos extent) coord
+                Nothing -> Nothing
+        
+        swapY (Z :. y :. x) = (x, (h - y))
+
+
+saveBMP :: Storable a => FilePath -> (a -> RGBPixel) -> Manifest a -> IO ()
+saveBMP path f img = 
+    writeBitmap path $ toJuicyRGB $ I.map f img
+
+saveRGB :: FilePath -> Manifest RGBPixel -> IO ()
+saveRGB path img = 
+    writeBitmap path $ toJuicyRGB img
+
+-- mergeTwo ::
+--     QuadTree (Point, Size, Int) ->
+--     QuadTree (Point, Size, Int)
+--     -> (QuadTree (Point, Size, Int), QuadTree (Point, Size, Int))
+
+-- mergeTwo (TNode) = T
+
+-- regionMerge :: QuadTree (Point, Size, Int) -> QuadTree (Point, Size, Int)
+-- regionMerge = undefined
 
 regionSplit :: Manifest Int -> QuadTree (Point, Size, Int)
-regionSplit img = makeTree (ix2 0 0) imgSize 0
+regionSplit img = fst $ makeTree (ix2 0 0) imgSize 0
     where
-        imgSize@(Z :. imgHeight :. imgWidth) = (manifestSize img)
-        makeTree :: Point -> Size -> Int -> QuadTree (Point, Size, Int)
-        makeTree point@(Z :. y :. x) size@(Z :. h :. w) prevSegment
-            | h == 1 && w == 1 = TLeaf (point, size, prevSegment + 1)
+        imgSize@(Z :. imgHeight :. imgWidth) = manifestSize img
+        makeTree :: Point -> Size -> Int -> (QuadTree (Point, Size, Int), Int)
+        makeTree point@(Z :. y :. x) size@(Z :. h :. w) currentSegment 
+            | y < 0 || x < 0 || y + h  > imgHeight || x + w > imgWidth = error "Invalid region"
+            | h == 1 || w == 1 = (TLeaf (point, size, currentSegment), currentSegment + 1)
             | otherwise =
                 let region = crop (Rect x y w h) img
-                    halfH = ceiling (fromIntegral h / 2)
-                    halfW = ceiling (fromIntegral w / 2)
-                    topLeft = makeTree point (ix2 halfH halfW) prevSegment
-                    topRight = makeTree (point + (Z :. 0 :. halfW)) (ix2 halfH halfW) (prevSegment + 1)
-                    bottomLeft = makeTree (point + (Z :. halfH :. 0)) (ix2 halfH halfW) (prevSegment + 2)
-                    bottomRight = makeTree (point + (Z :. halfH :. halfW)) (ix2 halfH halfW) (prevSegment + 3)
-                in  
+                    halfH = ceiling (fromIntegral h / 2 :: Double)
+                    halfW = ceiling (fromIntegral w / 2 :: Double)
+                    (nw, segmentN1) = makeTree point (ix2 halfH halfW) currentSegment
+                    (ne, segmentN2) = makeTree (ix2 y (x + halfW)) (ix2 halfH  (w - halfW)) segmentN1
+                    (sw, segmentN3) = makeTree (ix2 (y + halfH) x) (ix2 (h - halfH) halfW) segmentN2
+                    (se, segmentN4) = makeTree (ix2 halfH halfW) (ix2 (h - halfH) (w - halfW)) segmentN3
+                in 
                     if isHomogeneous region
-                    then TLeaf (point, size, prevSegment + 1)
-                    else
-                        if
-                        TNode topLeft topRight bottomLeft bottomRight
+                    then (TLeaf (point, size, currentSegment), currentSegment + 1)
+                    else (TNode nw ne sw se, segmentN4)
+
+            -- | h == 1 && w == 1 = TLeaf (point, size, prevSegment + 1)
+            -- | otherwise =
+
+            --             TNode topLeft topRight bottomLeft bottomRight
